154acb80f243bf59bcaeec3396db71612c0dbef1
Taylor Roberts <taylorlroberts7@gmail.com>
2019-07-18 11:58:08 -0600

    Fine tune styling

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,51 +1,69 @@
 import * as React from 'react';
 import { database } from '../firebase';
+import { RouteComponentProps } from 'react-router';
 
 const { useState } = React;
 
-// TODO: change any type of props
-const Welcome: React.FC = (props: any) => {
+const Welcome: React.FC<RouteComponentProps> = (props) => {
 
+    const [error, setError] = useState<string | null>(null);
     const [username, setUsername] = useState<string | null>(null);
 
     return (
         <div className="welcome">
-            <h1>WELCOME</h1>
+            <div>
+                <h1>WELCOME</h1>
+                <span>Enter username to chat it up &darr;</span>
+            </div>
             <form
                 className="username-form"
-                onSubmit={e => handleUsernameSubmit(e, username, props.history)}
+                onSubmit={e => handleUsernameSubmit(e, username, props.history, setError)}
             >
                 <input
-                    onChange={e => setUsername(e.target.value)}
+                    aria-label="Username"
+                    onChange={e => handleUsernameChange(e, setUsername, setError)}
                     placeholder="username"
                     type="text"
                 />
-                {/* <label>
-                    Username
-                    <input
-                        onChange={e => setUsername(e.target.value)}
-                        placeholder="username"
-                        type="text"
-                    />
-                </label> */}
+                <span>{error}</span>
                 <button>Start Chattin</button>
             </form>
         </div>
     );
 
-}
+};
 
-// TODO: CHANGE ANY TYPE FOR HISORY
-const handleUsernameSubmit = (event: React.FormEvent, username: string | null, history: any) => {
+const handleUsernameChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    setUsername: React.Dispatch<React.SetStateAction<string | null>>,
+    setError: React.Dispatch<React.SetStateAction<string | null>>
+) => {
 
-    // TODO: handle validation
+    setUsername(event.target.value);
+    setError(null);
+
+};
+
+const handleUsernameSubmit = (
+    event: React.FormEvent,
+    username: string | null,
+    history: RouteComponentProps['history'],
+    setError: React.Dispatch<React.SetStateAction<string | null>>
+) => {
 
     event.preventDefault();
+
+    if (!username) {
+
+        const error = 'Hold up. Please enter a username to talk to the cool cats.';
+        return setError(error);
+
+    }
 
     sessionStorage.setItem('username', username!);
     database.ref('users').push({ username });
     history.push('/chat');
 
-}
+};
 
 export default Welcome;
